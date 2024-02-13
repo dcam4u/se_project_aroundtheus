335c1010160d6630582112082aee5a8cd20f1200
Deanna C <deanna.camacho@icloud.com>
2024-02-13 11:01:00 -0700

    reviewer updates completed - round 1

--- scripts/index.js
+++ scripts/index.js
@@ -40,6 +40,7 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+
 cardListElement = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
@@ -48,7 +49,7 @@ const cardTemplate =
  *                             FUNCTIONS
  *========================================================================**/
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -83,7 +84,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileModalCloseButton.addEventListener("click", closePopup);
